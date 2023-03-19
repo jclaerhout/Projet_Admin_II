73aaa42634741b4d2a1629fe546a1c4fa0e18846
Justin Hespel Louis <93406828+JHespelLouis@users.noreply.github.com>
2023-03-19 18:27:49 +0100

    Web server + DB request

---
+++ Woodytoys/db/data.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Produits (
+    prodId INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nomProduit varchar(255) NOT NULL,
+    prodPrix decimal(6,2) NOT NULL
+);
+
+INSERT INTO Produits (nomProduit, prodPrix)
+VALUES ('table de ping pong', 5),
+        ('petites voitures', 6.50),
+        ('chaise à bascule', 25),
+        ('ballon de football', 10),
+        ('Toupies', 3.25);
